c7b739411081f510c723da99e8e668bfca20a4ab
Hiba <hibashareefamk@gmail.com>
2026-03-27 15:49:38 +0530

    push

--- Backend/app/inventory/views.py
+++ Backend/app/inventory/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from .models import Product
+from app.inventory.seriliazers import ShipmentTask
+from .seriliazers import ShipmentTaskSerializer,ProductSerializer
 
-# Create your views here.
+class ManagerAssignmentView(generics.CreateAPIView):
+    serializer_class = ShipmentTaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(manager=self.request.user)
+
+class StaffTaskListView(generics.ListAPIView):
+    serializer_class = ShipmentTaskSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return ShipmentTask.objects.filter(staff_assigned=user, department=user.department)
+    
+class Productview(APIView):
+    serializer = ProductSerializer
